9d6d5c0f168bb745308fbfede3a3b06d614003bb
MohAnanas <mohamed.belkacem@epitech.eu>
2018-12-11 13:44:22 +0100

    market place dome

--- app/src/main/java/org/gradle/chronosymple/MarketPlaceActivity.java
+++ app/src/main/java/org/gradle/chronosymple/MarketPlaceActivity.java
@@ -1,14 +1,24 @@
 package org.gradle.chronosymple;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 public class MarketPlaceActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.market_place);
 
+        final Intent intent = new Intent().setClass(this, HomePage.class);
+        Button dia = (Button) findViewById(R.id.Diabutton);
+        dia.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
