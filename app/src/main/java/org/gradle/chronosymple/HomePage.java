bb513d585ad060fbc25f485dd307547d18a743a7
Marie-Aimée <marie-aimee.fourtane@epitech.eu>
2018-12-11 11:57:37 +0100

    starting marketplace

--- app/src/main/java/org/gradle/chronosymple/HomePage.java
+++ app/src/main/java/org/gradle/chronosymple/HomePage.java
@@ -1,12 +1,34 @@
 package org.gradle.chronosymple;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class HomePage extends AppCompatActivity {
+    Button marketplace = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        marketplace = findViewById(R.id.marketplace);
+        marketplace.setOnClickListener(clickListenerConnect);
+    }
+
+    private View.OnClickListener clickListenerConnect = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            goToMarketPlacePage();
+        }
+    };
+
+    public void goToMarketPlacePage() {
+        Intent i = new Intent(this, MarketPlaceActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/org/gradle/chronosymple/MarketPlaceActivity.java
@@ -0,0 +1,14 @@
+package org.gradle.chronosymple;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MarketPlaceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.market_place);
+
+    }
+}
