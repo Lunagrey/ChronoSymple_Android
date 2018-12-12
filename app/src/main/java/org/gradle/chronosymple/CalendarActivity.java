664217152f0ae2dbaf27e4b72d8984f5191fd94c
Seth <laura.pereira@epitech.eu>
2018-12-12 15:56:34 +0100

    ResumeActivity

--- app/src/main/java/org/gradle/chronosymple/CalendarActivity.java
+++ app/src/main/java/org/gradle/chronosymple/CalendarActivity.java
@@ -1,27 +1,30 @@
 package org.gradle.chronosymple;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CalendarActivity extends AppCompatActivity {
-
-    String hide;
-    String glycémie;
-    String glucide;
-    String insulineapresrepas;
-    String insulineavantrepas;
-    String insulineajeun;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar);
-
-        hide = (String) getIntent().getSerializableExtra("uiojp");
-        glycémie = (String) getIntent().getSerializableExtra("glycémie");
-        glucide = (String) getIntent().getSerializableExtra("glucide");
-        insulineapresrepas = (String) getIntent().getSerializableExtra("insulineapresrepas");
-        insulineavantrepas = (String) getIntent().getSerializableExtra("insulineavantrepas");
-        insulineajeun = (String) getIntent().getSerializableExtra("insulineajeun");
+        final Intent intent = new Intent().setClass(this, HomePage.class);
+        Button dia = (Button) findViewById(R.id.Diabutton);
+        dia.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getToResume();
+            }
+        });
     }
-}
+
+    public void getToResume() {
+        Intent i = new Intent(this, ResumeActivity.class);
+        i.putExtra("hide", "false");
+        startActivity(i);
+    }
+    }
+
+
---
+++ app/src/main/java/org/gradle/chronosymple/ResumeActivity.java
@@ -0,0 +1,17 @@
+package org.gradle.chronosymple;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Seth on 12/12/2018.
+ */
+
+public class ResumeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resume);
+
+    }
+}
